9dcfe479f4b3705bf41891a1b783521f6bf0061e
Artem <artem.ivashchenko.exe@gmail.com>
2023-11-24 12:44:54 +0200

    make build

---
+++ index.96add056.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){var t=document.createElement("span");e.prepend(t),t.append(t.nextSibling)}),e.addEventListener("click",function(e){if("SPAN"===e.target.tagName){var t=e.target.parentElement.querySelector("ul");t&&(t.hidden=!t.hidden)}});//# sourceMappingURL=index.96add056.js.map
+
+//# sourceMappingURL=index.96add056.js.map
